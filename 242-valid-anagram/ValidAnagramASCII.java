5467f7a70db17c37b15753a3ea43e606da782175
Mani <mani@aranmani.com>
2026-08-07 11:25:21 -0500

    Using array ASCII solution

---
+++ 242-valid-anagram/ValidAnagramASCII.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class ValidAnagramASCII {
+    public boolean isAnagram(String s, String t) {
+        //check for length
+        if(s.length() != t.length()) {
+            return false;
+        }
+
+        int [] count = new int[26];
+        for(char ch: s.toCharArray()) {
+            count[ ch - 'a']++;
+        }
+        for(char ch: t.toCharArray()) {
+            count[ ch -'a']--;
+            if(count[ch -'a'] < 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
